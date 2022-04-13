35511c9688fe0340cd6402a4b16d97eddcdc63f8
cyberpunkx0007 <85433671+Cyberpunk0070@users.noreply.github.com>
2022-04-13 12:54:03 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const Data={
+    name: 'default',
+    usn:'default',
+    College:'default',
+    Age:0,
+    Gender:'default',
+
+  }
+  const [input,setInput]=useState(Data);
+
+  
+const handleNameChange = event =>{
+  const newObj={
+    name: event.target.value,
+    usn: input.usn,
+    College: input.College,
+    Age: input.Age,
+    Gender: input.Gender,
+  }
+  setInput(newObj)
+
+}
+const handleUsnChange = event =>{
+  const newObj={
+    name: input.name,
+    usn: event.target.value,
+    College: input.College,
+    Age: input.Age,
+    Gender: input.Gender,
+  }
+  setInput(newObj)
+
+}
+const handleCollegeChange = event =>{
+  const newObj={...input,
+    
+    name: event.target.value,
+  }
+  setInput(newObj)
+
+}
+
+  // function handleNameChange(event) {
+  //   input=setInput(event.target.value);
+  //   Data.name=input;
+  // }
+  // function handleUsnChange(event) {
+  //   input=setInput(event.target.value);
+  //   Data.usn=input;
+  // }
+  // function handleCollegeChange(event) {
+  //   input=setInput(event.target.value);
+  //   Data.College=input;
+  // }
+  // function handleAgeChange(event) {
+  //   input=setInput(event.target.value);
+  //   Data.Age=input;
+  // }
+  // function handleGenderChange(event) {
+  //   input=setInput(event.target.value);
+  //   Data.Gender=input;
+  // }
+  // function handleSubmit(){
+    
+
+  // }
+  
   return (
     <div className="App">
-      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+        <form>
+        <input name="Name" onChange={handleNameChange}/> 
+        {/* <input name="USN"onChange={handleUsnChange}/>
+        <input name="College"onChange={handleCollegeChange}/>
+        <input name="Age"onChange={handleAgeChange}/>
+        <input name="Gender"onChange={handleGenderChange}/>
+        <button onClick={handleSubmit}>Submit</button> */}
+        <button type='submit' placeholder='Show name' onClick={}></button>
+        </form>
+        {/* <p>{Data}</p> */}
+       <p></p>
     </div>
   );
 }
